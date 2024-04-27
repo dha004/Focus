55f38fc77102c0e0897f86cce03dd2449392efcd
jxtran9 <146775556+jxtran9@users.noreply.github.com>
2024-04-27 15:26:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,20 @@ document.getElementById("started").addEventListener("click", function () {
     document.querySelector('h1').textContent = "Let's Get Started!";
     document.querySelector('p').textContent = "Choose an option to begin your focused learning session:";
     document.getElementById('function-options').classList.toggle('hidden');
+    document.getElementById('gazeData').classList.add('hidden'); // Hide gaze data initially
 });
 
-document.getElementById("start-timer").addEventListener("click", function() {
+document.getElementById("start-timer").addEventListener("click", function () {
     document.querySelector('h1').textContent = "Timer Function Activated!";
     document.querySelector('p').textContent = "Use this timer to manage your study sessions effectively.";
     startTimer();
-    // Code to start the timer function
+    document.getElementById('gazeData').classList.add('hidden'); // Hide gaze data when timer starts
 });
 
-document.getElementById("start-eye-tracker").addEventListener("click", function() {
+document.getElementById("start-eye-tracker").addEventListener("click", function () {
+    console.log("activated functions")
     document.querySelector('h1').textContent = "Eye Tracker Activated!";
     document.querySelector('p').textContent = "Please calibrate the eye tracker by following the on-screen instructions.";
-    startEyeTracker();  // Ensure this function is accessible from eyeTracker.js
+    startEyeTracker();
+    document.getElementById('gazeData').classList.remove('hidden'); // Show gaze data when eye tracker starts
 });
